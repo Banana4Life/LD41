c22b2af384ea1663f0003ac029f1e66bbb2db448
Phillip Schichtel <phillip@schich.tel>
2018-04-22 23:27:10 +0200

    stuff

---
+++ Assets/MaterialSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class MaterialSelector : MonoBehaviour {
+	
+	public Material[] Materials;
+	public bool RendererInChildren = false;
+
+	void Start () {
+		var selection = Materials[Random.Range(0, Materials.Length)];
+		Debug.LogWarning("Selection:");
+		Debug.LogWarning(selection);
+		Renderer target;
+		if (RendererInChildren)
+		{
+			target = GetComponentInChildren<Renderer>();
+		}
+		else
+		{
+			target = GetComponent<Renderer>();
+		}
+		target.material = selection;
+	}
+}
