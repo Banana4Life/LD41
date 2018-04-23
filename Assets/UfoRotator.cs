6c9f62e0d46eb82d4fc2efe3f14b4237d7fea6c4
Phillip Schichtel <phillip@schich.tel>
2018-04-23 10:21:47 +0200

    stuff and things

---
+++ Assets/UfoRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UfoRotator : MonoBehaviour
+{
+	public float Scaler = 1;
+	
+	void Update ()
+	{
+		var agent = GetComponentInParent<NavMeshAgent>();
+		if (agent)
+		{
+			transform.RotateAround(Vector3.up, agent.velocity.magnitude * Scaler * Time.deltaTime);
+		}
+	}
+}
