b6b537d48acfc8035f98a35c478d70e291deccdf
Phillip Schichtel <phillip@schich.tel>
2018-04-23 19:58:20 +0200

    display point mode

---
+++ Assets/Scripts/PointModeDisplay.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class PointModeDisplay : MonoBehaviour
+{
+
+	public Game Game;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Text text = GetComponent<Text>();
+
+		if (Game.runSimulation)
+		{
+			text.text = "";
+		}
+		else
+		{
+			switch (Game.SelectedPointMode)
+			{
+				case PointMode.SPEEDUP:
+					text.text = "Getting faster!";
+					break;
+				case PointMode.SLOWDOWN:
+					text.text = "Getting slower!";
+					break;
+				case PointMode.SUSTAIN:
+					text.text = "Keeping speed!";
+					break;
+			}
+		}
+	}
+}
